8f4dd1e203742c452115120d0d66c609990765b8
Jeffrey Gillis <jeffrey.gillis1@gmail.com>
2019-04-09 22:16:45 -0400

    Create directories and render templates

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 import "os"
 import "strings"
 import "path/filepath"
-
-//import "gopkg.in/yaml.v2"
+import "gopkg.in/yaml.v2"
+import "html/template"
 
 const outputPath string = "./content"
 
@@ -15,8 +15,11 @@ type Recording struct {
 	Author   string   `yaml:author`
 	Tags     []string `yaml:tags`
 	FileName string
+	FilePath string
+	BaseName string
 }
 
+//Create a new Recording based on given filePath
 func newFromFilePath(filePath string) *Recording {
 	data := make([]byte, 200)
 
@@ -33,14 +36,19 @@ func newFromFilePath(filePath string) *Recording {
 	}
 
 	r := Recording{FileName: filePath}
+	r.BaseName = r.fileWithoutExt()
 
 	err = yaml.Unmarshal(data[:count], &r)
 	if err != nil {
 		panic(fmt.Errorf("There was an error reading file: %s with error: %v", filePath, err))
 	}
 
-	//r.FileName = filePath
 	return &r
+}
+
+func (r *Recording) fileWithoutExt() string {
+	name := strings.TrimSuffix(r.FileName, filepath.Ext(r.FileName))
+	return name
 }
 
 func (r *Recording) datePath() string {
@@ -58,15 +66,16 @@ func (r *Recording) createFilePath() {
 	current, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
+		panic(fmt.Errorf("Could not get working directory"))
 		os.Exit(1)
 	}
 
 	path := filepath.Join(current, outputPath, "recordings", r.datePath())
-	fmt.Println(path)
+	r.FilePath = path
 	err = os.MkdirAll(path, os.ModePerm)
 
 	if err != nil {
-		fmt.Println(err)
+		panic(fmt.Errorf("Could not create directory at path: %s, Error: %s", r.FilePath, err))
 		os.Exit(1)
 	}
 }
@@ -74,34 +83,51 @@ func (r *Recording) createFilePath() {
 func globFiles() []string {
 	files, err := filepath.Glob("./recordings/*.yaml")
 	if err != nil {
-		fmt.Println(err)
+		panic(fmt.Errorf("There was an error trying to retrieve yaml files: %s", err))
 		os.Exit(1)
 	}
 	return files
 }
 
+func renderTemplate(recordings []Recording) {
+	filePath := filepath.Join(recordings[0].FilePath, "index.html")
+	fmt.Println("Creating: ", filePath)
+	file, err := os.Create(filePath)
+	if err != nil {
+		panic(fmt.Errorf("There was an error creating file: %s, Error: %s", filePath, err))
+	}
+
+	defer file.Close()
+
+	tmpl, err := template.ParseFiles("./templates/recordings.html")
+	if err != nil {
+		panic(fmt.Errorf("There was an error loading the template: %s", err))
+	}
+	err = tmpl.Execute(file, recordings)
+	if err != nil {
+		panic(fmt.Errorf("There was an error rendering the template: %s", err))
+	}
+}
+
 func main() {
 
-	var recordings []Recording
+	var recordings = map[string][]Recording{}
 
 	files := globFiles()
 
-	/* convert into []map[string][]Recording{
-		{
-			"2019/01/01": []Recording{r1, r2}
-		}
+	/* convert into map[string][]Recording{
+		"2019/01/01": []Recording{r1, r2}
 	}
 	*/
 	for _, file := range files {
-		//r := Recording{}
-		fmt.Printf("Converting file: %s\n", file)
+		fmt.Printf("Processing: %s\n", file)
 		r := newFromFilePath(file)
-		fmt.Println("Recording: ", r.FileName, "Date: ", r.datePath())
-		fmt.Println("Creating output directory: ", r.FileName)
 		r.createFilePath()
-		recordings = append(recordings, *r)
+		recordings[r.datePath()] = append(recordings[r.datePath()], *r)
 	}
 
-	fmt.Println("Recording Count: ", len(recordings))
+	for _, records := range recordings {
+		renderTemplate(records)
+	}
 
 }
